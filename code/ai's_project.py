378009575794b3a33333acee2042a2066d726f69
Federico0307 <76046251+Federico0307@users.noreply.github.com>
2024-07-29 20:52:39 +0200

    Update ai's_project.py

--- code/ai's_project.py
+++ code/ai's_project.py
@@ -12,7 +12,6 @@ The following code is related to the project realised for the Artificial Intelli
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
